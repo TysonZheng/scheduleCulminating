b5fe889e24bf47b0372263c25e639a75ccccdc30
morgansunn <81695232+morgansunn@users.noreply.github.com>
2021-05-19 12:45:05 -0400

    Update App.java

--- finals/src/App.java
+++ finals/src/App.java
@@ -1,41 +1,18 @@
 import javafx.application.Application;
 import javafx.scene.*;
 import javafx.stage.*;
-import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.paint.Color;
-import javafx.scene.image.*;
+import javafx.fxml.FXMLLoader;
 
-public class App extends Application implements EventHandler<ActionEvent>{
+public class App extends Application {
 
-    Stage window;
-    Button button;
-
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Student Reminder App");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
     public static void main(String[] args) throws Exception {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.PINK);
-        
-        Image icon = new Image("icon.png");
-        stage.getIcons().add(icon);
-        stage.setTitle("Student Reminder App");
-
-        stage.setWidth(1500);
-        stage.setHeight(900);
-
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
-    @Override
-    public void handle(ActionEvent event) {
-        // if(event.getSource())
     }
 }
