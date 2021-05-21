de2835b4afe85fcc169e306fc264c8e78dfb7ec4
Tyson <335494555@gapps.yrdsb.ca>
2021-05-21 09:46:15 -0400

    Set clock/ timer
    -Created new date and timer objects as well as formatted

--- finals/src/schedulerCulminating.java
+++ finals/src/schedulerCulminating.java
@@ -2,9 +2,10 @@ import javafx.application.Application;
 import javafx.scene.*;
 import javafx.stage.*;
 import javafx.fxml.FXMLLoader;
+import java.time.LocalDate;
 
 public class schedulerCulminating extends Application {
-
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
@@ -12,7 +13,13 @@ public class schedulerCulminating extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+    
     public static void main(String[] args) throws Exception {
+        javaTimer();
         launch(args);
+    }
+    public static void javaTimer(){
+        LocalDate date = LocalDate.now();
+        System.out.println(date); 
     }
 }
