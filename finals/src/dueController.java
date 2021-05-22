b3d3e4af607279fb1b6e2a5a1344258f3ddabd21
morgansunn <81695232+morgansunn@users.noreply.github.com>
2021-05-22 16:37:41 -0400

    Update dueController.java

--- finals/src/dueController.java
+++ finals/src/dueController.java
@@ -1,11 +1,29 @@
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 
-public class dueController {
+public class duefController implements Initializable {
+    
+    @FXML
+    private TextField task;
 
     @FXML
-    void toName(ActionEvent event) {
+    private TextField date;
+
+    @FXML
+    void switchScene1(ActionEvent event) {
         
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+    }
+   
 }
