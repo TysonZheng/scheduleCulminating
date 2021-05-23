64b77d1d7f46d146d8d6bdcaa1d0e962467daf58
Chong-Kyle <335545380@gapps.yrdsb.ca>
2021-05-23 17:24:12 -0400

    Added basic I/O + csv file
    Added basic/temporary user input using scanner for task, day, month, and year. Added file output which appends each element into 'schedule.csv'

--- finals/src/schedulerCulminating.java
+++ finals/src/schedulerCulminating.java
@@ -7,9 +7,17 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class schedulerCulminating extends Application {
-    
+    private static String task, day, month, year;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
@@ -21,6 +29,8 @@ public class schedulerCulminating extends Application {
     public static void main(String[] args) throws Exception {
         javaTimer();
         launch(args);
+        fileInput();
+        fileOutput();
     }
     public static void javaTimer(){
         Timer timer = new Timer();
@@ -40,5 +50,44 @@ public class schedulerCulminating extends Application {
             }  
         }, 0, 1000);
     }
-    public static void  
+    public static void fileInput(){
+        
+        //SCANNER INPUT IS TEMPORARY SINCE IT WILL BE IMPLEMENTED INTO GUI
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter the task name");
+        task = reader.nextLine();
+        System.out.println("Enter the Day (Integer)");
+        day = reader.nextLine();
+        System.out.println("Enter the month (Integer)");
+        month = reader.nextLine();
+        System.out.println("Enter the year (Integer)");
+        year = reader.nextLine();
+        reader.close();
+    }
+    public static void fileOutput(){
+        String filePath = "schedule.csv";
+        try {
+            FileWriter fw = new FileWriter(filePath, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.append(task);
+            pw.append(":");
+            pw.append(day);
+            pw.append("-");
+            pw.append(month);
+            pw.append("-");
+            pw.append(year);
+            pw.append("\n");
+            pw.flush();
+            pw.close();
+
+            System.out.println("ELEMENTS ADDED TO FILE");
+            
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
 }
