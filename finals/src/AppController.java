0e82eb319615b3af59a55ab4366892ae055db5b8
Chong-Kyle <335545380@gapps.yrdsb.ca>
2021-06-04 22:42:05 -0400

    Changed file path AppController
    destination csv file changed from schedule.csv to dueDates.csv

--- finals/src/AppController.java
+++ finals/src/AppController.java
@@ -76,7 +76,7 @@ public class AppController implements Initializable {
 
     @FXML
     public void onClickGen(ActionEvent E) throws IOException {
-        File file = new File("schedule.csv");
+        File file = new File("dueDates.csv");
         FileWriter fw = new FileWriter(file);
 
         fw.write(TASK.getText());
