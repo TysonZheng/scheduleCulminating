ffee28098445b851023d6249303ddb0f7c773eee
Chong-Kyle <335545380@gapps.yrdsb.ca>
2021-06-04 22:01:16 -0400

    Fixed taskRemover
    changed println to print + print(\n)

--- finals/src/App.java
+++ finals/src/App.java
@@ -325,7 +325,8 @@ public class App extends Application {
                 if (!foundTask.equals(searchTask)) {
                     foundInformation = foundTask+":"+foundYear+"-"+foundMonth+"-"+foundDay+"-"+foundHour+":"+foundMinute;
                     System.out.println(foundInformation);
-                    pw.println(foundInformation);
+                    pw.print(foundInformation);
+                    pw.print("\n");
                 }
             }
             fileReader.close();
